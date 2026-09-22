55ff2f46dcec46bff27652aae47ebadd81d067d9
Ryouf <ryouff87@gmail.com>
2026-09-22 15:09:31 +0300

    Complete Lab 3 Django MVT

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+# from django.urls import path
+# import apps.bookmodule.views 
+from django.urls import include, path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('', apps.bookmodule.views.index),
+    # path('index2/<int:val1>/', apps.bookmodule.views.index2),
+    path('users/', include("apps.usermodule.urls")),
+    path('books/', include("apps.bookmodule.urls")),
+    
 ]
